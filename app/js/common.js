17babe492e7b86b7de21e15f8c57426c138fb4b1
Yulia Bicheva <juliebycheva@gmail.com>
2017-06-19 02:38:19 +0300

    Created opened animated menu using plugin Mmenu.

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,19 @@
 $(function() {
+	$('#my-menu').mmenu({
+		extensions: [ 'widesceen', 'theme-black', 'fx-menu-slide', 'pagedim-black'],
+		navbar : {
+			title: '<img src="img/logo1.svg" alt="Салон красоты Смилтер"/>'
+		},
+		offCanvas: {
+           position  : 'right'
+        }
+	});
 
-	// Custom JS
+	var api = $('#my-menu').data( "mmenu" );
 
+	api.bind('open:start', function() {
+		$('.hamburger').addClass('is-active')
+	}).bind('close:start', function() {
+		$('.hamburger').removeClass('is-active')
+	});
 });
