be52cb6b1a9e5130b09b98121f670dd14ceb35be
Yulia Bicheva <juliebycheva@gmail.com>
2017-06-20 15:38:24 +0300

    Created composition at the header. Deleted unnecessary files.

--- app/js/common.js
+++ app/js/common.js
@@ -11,9 +11,9 @@ $(function() {
 
 	var api = $('#my-menu').data( "mmenu" );
 
-	api.bind('open:start', function() {
+	api.bind('open:finish', function() {
 		$('.hamburger').addClass('is-active')
-	}).bind('close:start', function() {
+	}).bind('close:finish', function() {
 		$('.hamburger').removeClass('is-active')
 	});
 });
